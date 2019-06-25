051c1a4fce92c9b1067113fb237a3005dc301342
Nikolay <laveosa@yahoo.com>
2019-06-25 16:17:10 +0300

    Updates

---
+++ precache-manifest.ea901a04c58031cc69991441238eb93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520e5721abe52a9cc46ba30a51227967",
+    "url": "/pro_one/index.html"
+  },
+  {
+    "revision": "71f859b6e67e676ec5bb",
+    "url": "/pro_one/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "cff4e229df5e07ef83bf",
+    "url": "/pro_one/static/css/main.4650eb4b.chunk.css"
+  },
+  {
+    "revision": "71f859b6e67e676ec5bb",
+    "url": "/pro_one/static/js/2.1188c15e.chunk.js"
+  },
+  {
+    "revision": "cff4e229df5e07ef83bf",
+    "url": "/pro_one/static/js/main.37fcd45a.chunk.js"
+  },
+  {
+    "revision": "0f51b6f545cccd7295cf",
+    "url": "/pro_one/static/js/runtime~main.2b279ad9.js"
+  }
+]);
