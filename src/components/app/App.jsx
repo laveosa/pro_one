702f299bcaf495552ac307e0732fdfefd8752a07
Nikolay Grygoryev <nikolay@barnyardcreative.com>
2019-06-30 17:32:14 +0300

    set start flow

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="AppComponent" className="app-container">
+        <h1>App page</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
